f6f2d919091b6f32b23bcc3abd7e94e4d4372963
nahyyun <nahyuun@naver.com>
2023-08-22 21:20:59 +0900

    fix: cors 해결v3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
+
+const getCorsHeaders = () => {
+  const headers = {};
+
+  headers["Access-Control-Allow-Origin"] = "*";
+  headers["Access-Control-Allow-Credentials"] = "true";
+  headers["Access-Control-Allow-Methods"] = "GET,OPTIONS,PATCH,DELETE,POST,PUT";
+  headers["Access-Control-Allow-Headers"] =
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization";
+
+  return Object.entries(headers).map(([key, value]) => ({ key, value }));
+};
+
 const nextConfig = {
   experimental: {
     appDir: true,
+  },
+  headers: async () => {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: getCorsHeaders(),
+      },
+    ];
   },
   images: {
     remotePatterns: [
