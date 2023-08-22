978432f4ce4fa28a9375f964f145634defd2f643
nahyyun <nahyuun@naver.com>
2023-08-22 18:38:21 +0900

    project done

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,11 +3,3 @@ import { format } from "timeago.js";
 export function parseDate(date: string) {
   return format(date);
 }
-
-// async function promise() {
-//   new Promise((resolve, reject) => {
-//     return setTimeout(() => resolve("success"), 5000);
-//   }).then((res) => console.log(res));
-// }
-
-// promise().then((res) => console.log("res", res));
