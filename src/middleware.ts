3c6779b7fd65471642cf98cd8d731ae3376954b3
nahyyun <nahyuun@naver.com>
2023-08-22 21:05:45 +0900

    fix: cors 해결

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,14 @@ export function middleware(req: NextRequest) {
 
   if (!token) {
     if (req.nextUrl.pathname.startsWith("/api")) {
-      return new NextResponse("Authentication Error", { status: 401 });
+      return new NextResponse("Authentication Error", {
+        status: 401,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+      });
     }
 
     const { origin, pathname, basePath, search } = req.nextUrl;
@@ -21,7 +28,14 @@ export function middleware(req: NextRequest) {
     return NextResponse.redirect(signInUrl);
   }
 
-  return NextResponse.next();
+  return NextResponse.next({
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
 }
 
 export const config = {
