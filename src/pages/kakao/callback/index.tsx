72e4ad2014e75ee21c8cc8d83e277dffa1c92432
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-24 22:01:27 +0900

    feat: 로그인 콜백 페이지 구현

---
+++ src/pages/kakao/callback/index.tsx
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+
+export default function Redirect() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(router.query.code);
+      router.push('/');
+    }, 1000);
+  }, []);
+
+  return <div>로그인 중 입니다...</div>;
+}
