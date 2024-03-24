b6e49c1607b92731eaed1e9771e1ed9abca9da20
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-24 17:07:06 +0900

    feat: 홈 페이지 UI 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
-import { PretendardBold, PretendardRegular, PretendardSemiBold } from '@styles/font';
 import Head from 'next/head';
+import Logo from '../asset/logo.svg';
+import Button from '@components/common/Button';
+import Image from 'next/image';
+import { box, header, highlight, image, logo, subtitle, text_box, wrapper } from './style';
+import { title } from 'process';
 
 export default function Home() {
   return (
@@ -7,17 +11,42 @@ export default function Home() {
       <Head>
         <title>ZIPup | 집들이 선물 펀딩 서비스</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <style>
+          {`
+            :root {
+              --font-pretendard-bold: 'Pretendard-Bold', sans-serif;
+              --font-pretendard-semibold: 'Pretendard-SemiBold', sans-serif;
+              --font-pretendard-regular: 'Pretendard-Regular', sans-serif;
+            }
+          `}
+        </style>
       </Head>
-      <style>
-        {`
-          :root {
-            --font-pretendard-bold: ${PretendardBold.style.fontFamily};
-            --font-pretendard-semibold: ${PretendardSemiBold.style.fontFamily};
-            --font-pretendard-regular: ${PretendardRegular.style.fontFamily};
-          }
-        `}
-      </style>
-      <main />
+      <main>
+        <header className={header}>
+          <div className={box} />
+          <button className={logo}>
+            <Logo width={72.7} height={28} />
+          </button>
+          <div className={box} />
+        </header>
+        <div className={text_box}>
+          <p className={title}>
+            조금씩 마음을 보태어 <span className={highlight}>집들이 선물</span>을 보내요
+          </p>
+          <span className={subtitle}>
+            더나은 집들이 경험을 위한
+            <br />
+            집들이 선물 펀딩 서비스
+          </span>
+        </div>
+        <div className={wrapper}>
+          <div className={image}>
+            <Image src={''} alt="" />
+          </div>
+          <Button text="내 펀딩을 만들어볼까요?" color="primary" onClick={() => null} />
+          <Button text="서비스 둘러볼게요" color="secondary" onClick={() => null} />
+        </div>
+      </main>
     </>
   );
 }
