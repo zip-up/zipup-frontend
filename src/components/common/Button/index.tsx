eeedeb86af32c44b66fe040d839c0034dfb2916e
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-24 17:06:53 +0900

    feat: 버튼 공통 컴포넌트 구현

---
+++ src/components/common/Button/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import classnames from 'classnames';
+import { button, styles } from './styles';
+import { css } from '../../../../styled-system/css';
+
+interface ButtonProps {
+  onClick: () => void;
+  text: string;
+  height?: string;
+  color: 'primary' | 'secondary' | 'disabled';
+}
+
+function Button({ height = '5.2rem', onClick, text, color }: ButtonProps) {
+  return (
+    <button className={classnames(button, styles[color], css({ height }))} onClick={onClick}>
+      {text}
+    </button>
+  );
+}
+
+export default Button;
---
+++ src/components/common/Button/styles.ts
@@ -0,0 +1,34 @@
+import { css } from '../../../../styled-system/css';
+
+const button = css({
+  height: '5.2rem',
+  width: '100%',
+  borderRadius: '0.8rem',
+  color: '#FFF',
+  fontFamily: 'pretendard-semibold',
+  fontSize: 'body1',
+  fontWeight: '600',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  cursor: 'pointer',
+});
+
+const styles = {
+  primary: css({
+    backgroundColor: 'main.blue',
+    '&:hover': { backgroundColor: 'blue.40' },
+  }),
+
+  secondary: css({
+    backgroundColor: 'text.100',
+    '&:hover': { backgroundColor: 'text.200' },
+  }),
+
+  disabled: css({
+    backgroundColor: 'text.300',
+    cursor: 'default',
+  }),
+};
+
+export { button, styles };
