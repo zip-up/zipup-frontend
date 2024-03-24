159ada0a75f067bf506a405cd29daf1a63bee74f
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-24 23:32:56 +0900

    fix: text를 children으로 변경

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,18 +1,23 @@
 import classnames from 'classnames';
 import { button, styles } from './styles';
 import { css } from '../../../../styled-system/css';
+import { PropsWithChildren } from 'react';
 
 interface ButtonProps {
   onClick: () => void;
-  text: string;
   height?: string;
   color: 'primary' | 'secondary' | 'disabled';
 }
 
-export default function Button({ height = '5.2rem', onClick, text, color }: ButtonProps) {
+export default function Button({
+  height = '5.2rem',
+  onClick,
+  color,
+  children,
+}: PropsWithChildren<ButtonProps>) {
   return (
     <button className={classnames(button, styles[color], css({ height }))} onClick={onClick}>
-      {text}
+      {children}
     </button>
   );
 }
