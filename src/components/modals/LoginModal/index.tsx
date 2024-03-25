00d57fac34b054c4290d72c5bc5b29b7dcd179d5
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-25 17:32:28 +0900

    [#10] Feature: 로그인 기능 api 연결 (#12)
    * fix: 버튼 컴포넌트 수정에 따른 코드 수정
    
    * remove: 불필요한 파일 삭제
    
    * feat: 카카오 로그인 API 연결

--- src/components/modals/LoginModal/index.tsx
+++ src/components/modals/LoginModal/index.tsx
@@ -1,5 +1,5 @@
 import Modal from '@components/common/Modal';
-import Login from '@asset/login-button.svg';
+import Login from '@assets/login-button.svg';
 import * as style from './styles';
 
 interface LoginModalProps {
@@ -7,12 +7,8 @@ interface LoginModalProps {
 }
 
 export default function LoginModal({ onClose }: LoginModalProps) {
-  const REDIRECT_URI = 'http://localhost:3000/kakao/callback';
-  const url = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code
-`;
-
-  const handleLogin = () => {
-    window.location.href = url;
+  const handleLoginClick = () => {
+    window.location.href = process.env.NEXT_PUBLIC_BASE_URL + '/oauth2/authorization/kakao';
   };
 
   return (
@@ -23,7 +19,7 @@ export default function LoginModal({ onClose }: LoginModalProps) {
       <p className={style.title}>로그인이 필요한 서비스입니다.</p>
       <p className={style.subtitle}>카카오 로그인으로 5초만에 시작해요!</p>
       <p className={style.button_box}>
-        <button className={style.button} onClick={handleLogin}>
+        <button className={style.button} onClick={handleLoginClick}>
           <Login />
         </button>
       </p>
