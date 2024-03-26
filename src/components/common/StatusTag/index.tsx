049ac332b25f5c4f1f3c22cdf67f2dc69841f6d5
nahyyun <nahyuun@naver.com>
2024-03-27 00:23:09 +0900

    feat: floating 용 tag컴포넌트를 위한 속성 추가 (#15)

--- src/components/common/StatusTag/index.tsx
+++ src/components/common/StatusTag/index.tsx
@@ -3,15 +3,22 @@ import * as style from './styles';
 interface StatusTagProps {
   isCompleted?: boolean;
   daysLeft?: number;
+  isFloating?: boolean;
 }
 
-export default function StatusTag({ isCompleted = false, daysLeft = 0 }: StatusTagProps) {
+export default function StatusTag({
+  isCompleted = false,
+  daysLeft = 0,
+  isFloating = false,
+}: StatusTagProps) {
+  const size = isFloating ? 'floating' : 'static';
+
   return (
     <>
       {isCompleted ? (
-        <div className={style.statusTag({ bg: 'black' })}>완료</div>
+        <div className={style.statusTag({ bg: 'black', size })}>완료</div>
       ) : (
-        <div className={style.statusTag({ bg: 'blue' })}>D-{daysLeft}</div>
+        <div className={style.statusTag({ bg: 'blue', size })}>D-{daysLeft}</div>
       )}
     </>
   );
--- src/components/common/StatusTag/styles.ts
+++ src/components/common/StatusTag/styles.ts
@@ -2,15 +2,25 @@ import { cva } from '@styled-system/css';
 
 const statusTag = cva({
   base: {
-    rounded: '1.15rem',
+    rounded: '0.9rem',
     color: 'white',
-    p: '0.6rem 1.2rem',
     fontWeight: '600',
+    fontSize: 'caption1',
+    w: 'fit-content',
+    h: 'fit-content',
   },
   variants: {
     bg: {
       black: { bg: 'main.black' },
       blue: { bg: 'success' },
+    },
+    size: {
+      static: {
+        p: '0.6rem 1.2rem',
+      },
+      floating: {
+        p: '1rem 1.2rem',
+      },
     },
   },
 });
