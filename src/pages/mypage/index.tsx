29ec8a6d371b574ea54c0b44b584f5673cae9407
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-27 16:24:01 +0900

    feat: 마이페이지 UI 구현

---
+++ src/pages/mypage/index.tsx
@@ -0,0 +1,102 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import Button from '@components/common/Button';
+import HeaderWithLogo from '@components/HeaderWithLogo';
+import UserIcon from '@assets/user.svg';
+import GoIcon from '@assets/go.svg';
+import ExitIcon from '@assets/exit.svg';
+import MyFundingIcon from '@assets/my-funding.svg';
+import ParticipatedFundingIcon from '@assets/participated_funding.svg';
+import React, { useEffect } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import * as style from './styles';
+
+const MyPage = () => {
+  const router = useRouter();
+  const isLoggedIn = true;
+  const image = '';
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.push('/');
+    }
+  }, [isLoggedIn]);
+
+  return (
+    <>
+      <HeaderWithLogo />
+      <div className={style.content}>
+        <div className={style.profile_box}>
+          <div className={style.info_box}>
+            <div className={style.avatar}>
+              {image ? <Image src={''} alt="profile image" width={42} height={42} /> : <UserIcon />}
+            </div>
+            <div className={style.name_box}>
+              <span className={style.name}>김집업</span>
+              <span>님</span>
+            </div>
+            <button className={style.logout}>
+              <ExitIcon />
+            </button>
+          </div>
+          <Button
+            color="primary"
+            className={style.button}
+            onClick={() => router.push('/funding/create/1')}
+          >
+            내 펀딩 만들러 가기 <GoIcon />
+          </Button>
+        </div>
+        <div className={style.service_box}>
+          <span className={style.service_title}>서비스</span>
+          <button
+            className={style.go_funding_btn}
+            onClick={() => router.push('/mypage/fundings/my')}
+          >
+            <div className={style.go_funding_info_box}>
+              <h2 className={style.go_funding_title}>내가 만든 펀딩</h2>
+              <span className={style.go_funding_subtitle}>작성한 펀딩을 확인할 수 있어요</span>
+            </div>
+            <div className={style.funding_image}>
+              <MyFundingIcon />
+            </div>
+          </button>
+          <button
+            className={style.go_funding_btn}
+            onClick={() => router.push('/mypage/fundings/participated')}
+          >
+            <div className={style.go_funding_info_box}>
+              <h2 className={style.go_funding_title}>참여한 펀딩</h2>
+              <span className={style.go_funding_subtitle}>결제한 펀딩을 확인할 수 있어요</span>
+            </div>
+            <div className={style.funding_image}>
+              <ParticipatedFundingIcon />
+            </div>
+          </button>
+        </div>
+        <footer className={style.footer}>
+          <div className={style.footer_info_box}>
+            <p>상호명 : 집업</p>
+            <p>고객센터 : 0504-0815-5379</p>
+            <div className={style.terms_and_conditions}>
+              <a
+                href="https://www.figma.com/exit?url=https%3A%2F%2Fdanisong.notion.site%2F508a845508794eab98435cecea30d561%3Fpvs%3D4"
+                className={style.pointer}
+              >
+                이용약관
+              </a>
+              <a
+                href="https://danisong.notion.site/bdf9880b3f91458fbe1a4118de2b5eb1?pvs=4"
+                className={style.pointer}
+              >
+                개인정보처리방침
+              </a>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default MyPage;
---
+++ src/pages/mypage/styles.ts
@@ -0,0 +1,191 @@
+import { css } from '@styled-system/css';
+
+const content = css({
+  height: 'calc(100% - 4.8rem)',
+  backgroundColor: 'bg.200',
+  paddingTop: '2.4rem',
+});
+
+const profile_box = css({
+  width: '32.9rem',
+  height: '14.1rem',
+  margin: '0 auto',
+  borderRadius: '2rem',
+  borderWidth: '0.1rem',
+  borderColor: 'gray.30',
+  backgroundColor: 'white',
+  padding: '1.8rem 2.2rem',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.6rem',
+});
+
+const button = css({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.8rem',
+});
+
+const avatar = css({
+  width: '4.2rem',
+  height: '4.2rem',
+  borderRadius: '100%',
+  overflow: 'hidden',
+  marginRight: '2.4rem',
+});
+
+const info_box = css({
+  display: 'flex',
+  alignItems: 'center',
+});
+
+const name_box = css({
+  fontFamily: 'pretendard-semibold',
+  fontSize: 'title2',
+  fontWeight: '600',
+  color: 'text.200',
+  marginRight: '0.8rem',
+});
+
+const name = css({
+  color: 'text.100',
+  marginRight: '0.5rem',
+});
+
+const logout = css({
+  cursor: 'pointer',
+});
+
+const service_box = css({
+  marginTop: '3.2rem',
+  width: '36rem',
+  height: '25.9rem',
+  padding: '2.4rem 1.6rem',
+  backgroundColor: 'blue.10',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.6rem',
+});
+
+const service_title = css({
+  fontFamily: 'pretendard-semibold',
+  fontWeight: '600',
+  fontSize: 'body1',
+  color: 'text.200',
+});
+
+const go_funding_btn = css({
+  width: '32.8rem',
+  height: '8rem',
+  borderRadius: '0.8rem',
+  paddingLeft: '2.4rem',
+  backgroundColor: 'white',
+  borderWidth: '0.1rem',
+  borderColor: 'blue.20',
+  display: 'flex',
+  position: 'relative',
+  cursor: 'pointer',
+});
+
+const go_funding_info_box = css({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  gap: '0.4rem',
+  paddingTop: '1.1rem',
+});
+
+const go_funding_title = css({
+  fontFamily: 'pretendard-semibold',
+  color: 'text.100',
+  fontWeight: '600',
+  fontSize: 'subtitle2',
+  textAlign: 'left',
+});
+
+const go_funding_subtitle = css({
+  fontFamily: 'pretendard-regular',
+  color: 'text.300',
+  fontSize: 'body2',
+  textAlign: 'left',
+});
+
+const funding_image = css({
+  position: 'absolute',
+  right: 0,
+  bottom: '-0.08rem',
+});
+
+const footer = css({
+  height: '14.8rem',
+  width: '100%',
+  position: 'absolute',
+  bottom: 0,
+});
+
+const footer_info_box = css({
+  marginLeft: '0.8rem',
+  width: '16.9rem',
+  height: '10.6rem',
+  padding: '2.4rem 0.8rem',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.8rem',
+  fontFamily: 'pretendard-regular',
+  fontSize: 'caption1',
+  color: 'text.100',
+});
+
+const terms_and_conditions = css({
+  display: 'flex',
+  gap: '0.8rem',
+  fontFamily: 'pretendard-regular',
+  fontSize: 'caption1',
+  color: 'text.200',
+  textDecoration: 'underline',
+  textDecorationColor: 'text.200',
+  textUnderlineOffset: '0.2rem',
+});
+
+const pointer = css({
+  cursor: 'pointer',
+});
+
+const card_content = css({
+  height: 'calc(100% - 4.8rem)',
+  backgroundColor: 'bg.200',
+  padding: '1.6rem',
+});
+
+const flex_container = css({
+  width: '32.6rem',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  flexWrap: 'wrap',
+  overflow: 'hidden',
+});
+
+export {
+  content,
+  profile_box,
+  button,
+  avatar,
+  info_box,
+  name_box,
+  name,
+  logout,
+  service_box,
+  service_title,
+  go_funding_btn,
+  go_funding_info_box,
+  go_funding_subtitle,
+  go_funding_title,
+  funding_image,
+  footer,
+  footer_info_box,
+  terms_and_conditions,
+  pointer,
+  card_content,
+  flex_container,
+};
