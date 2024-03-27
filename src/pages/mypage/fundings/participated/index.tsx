418b392cdf40af90e3a965dc7eaf67543f1f62b0
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-27 16:24:15 +0900

    feat: 내가 만든/참여한 펀딩 페이지 UI 구현

---
+++ src/pages/mypage/fundings/participated/index.tsx
@@ -0,0 +1,41 @@
+import Card from '@components/Card';
+import Header from '@components/common/Header';
+import { useRouter } from 'next/router';
+import React from 'react';
+import * as style from '../../styles';
+
+const data = [
+  {
+    title: '펀딩 제목입니다',
+    amount: 0,
+  },
+  {
+    title: '펀딩 제목입니다',
+    amount: 25,
+  },
+  {
+    title: '펀딩 제목입니다',
+    amount: 50,
+  },
+  {
+    title: '펀딩 제목입니다',
+    amount: 100,
+  },
+];
+
+export default function ParticipatedFundings() {
+  const router = useRouter();
+
+  return (
+    <>
+      <Header hasTitle title="내가 참여한 펀딩" onGoBack={() => router.back()} />
+      <div className={style.card_content}>
+        <div className={style.flex_container}>
+          {data.map((item, index) => (
+            <Card key={index} data={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
