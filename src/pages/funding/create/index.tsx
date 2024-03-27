046309173b3c0d7682d30aadbd35c9ed10816a4d
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-27 19:28:42 +0900

    feat: 리다이렉션 추가

--- src/pages/funding/create/index.tsx
+++ src/pages/funding/create/index.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
 
 const CreateFunding = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/funding/create/1');
+  }, []);
+
   return <div>hi</div>;
 };
 
