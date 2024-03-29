0fdb99feaac2a2fb065b2f3aaaf38e6cd3b07b4a
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-29 19:13:49 +0900

    feat: 로그인 API 연결 및 유저 데이터 저장

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,5 +1,6 @@
 import { atom } from 'recoil';
 import { CreateFund } from './types';
+import { Auth, User } from '@typings/auth';
 
 export const createFundState = atom<CreateFund>({
   key: 'createFund',
@@ -29,5 +30,15 @@ export const fundingFormState = atom({
     senderName: '',
     price: 0,
     msg: '',
+  },
+});
+
+export const userState = atom<User>({
+  key: 'user',
+  default: {
+    id: '',
+    name: '',
+    email: '',
+    profileImage: '',
   },
 });
--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,6 +1,10 @@
-export type Auth = {
+export interface User {
   id: string;
   name: string;
   email: string;
   profileImage: string;
-};
+}
+
+export interface UserWithToken extends User {
+  accesstoken: string;
+}
