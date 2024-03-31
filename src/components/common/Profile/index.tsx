694005ebe48faf499e1d0a81ed2fc80847536a15
nahyyun <nahyuun@naver.com>
2024-03-31 18:55:41 +0900

    fix: header profile 사이즈 수정

--- src/components/common/Profile/index.tsx
+++ src/components/common/Profile/index.tsx
@@ -2,21 +2,22 @@ import { css } from 'styled-system/css';
 import Image from 'next/image';
 interface ProfileProps {
   src: string;
-  width: string;
-  height: string;
+  width?: string;
+  height?: string;
   isFull?: boolean;
 }
 
 export default function Profile({ src, width, height, isFull = false }: ProfileProps) {
-  console.log(width, height);
-
-  const size = isFull ? { width: '100%', height: '100%' } : { width, height };
+  const size = isFull || !width ? { width: '100%', height: '100%' } : { width, height };
 
   return (
-    <div className={css({ width: '5rem', height: '5rem', overflow: 'hidden' })}>
-      <img
+    <div className={css(size, { overflow: 'hidden' })}>
+      <Image
         src={src}
         alt="프로필 이미지"
+        width={0}
+        height={0}
+        sizes="100vw"
         style={{
           borderRadius: isFull ? '100%' : '5rem',
           objectFit: 'cover',
