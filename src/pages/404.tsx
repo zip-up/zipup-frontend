532a8604c05830637458e9d1ad4daf2e20cb488a
nahyyun <nahyuun@naver.com>
2024-03-31 09:39:00 +0900

    feat: error 페이지 내 메세지에 text align 속성 추가

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -27,10 +27,10 @@ export default function Custom404() {
           m: '1rem 0rem',
         })}
       >
-        <h1 className={css({ fontWeight: '600', fontSize: 'subtitle2' })}>
+        <h1 className={css({ fontWeight: '600', fontSize: 'subtitle2', textAlign: 'center' })}>
           찾으시는 페이지가 없습니다
         </h1>
-        <div className={css({ fontWeight: '400', fontSize: 'body2' })}>
+        <div className={css({ fontWeight: '400', fontSize: 'body2', textAlign: 'center' })}>
           <p>잘못된 접근이거나 요청하신 페이지를 찾을 수 없어요.</p>
           <p>입력하신 페이지의 주소가 정확한지 확인해주세요.</p>
         </div>
