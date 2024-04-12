625f7af6196b220e2e13e679ded13a9148bc3258
nahyyun <nahyuun@naver.com>
2024-04-12 17:29:09 +0900

    fix: 토큰 검색 함수 호출 시점 변경

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,13 +2,11 @@ import { getLoacalStorage, setLocalStorage } from '@store/localStorage';
 import axios from 'axios';
 import { getNewToken } from './auth';
 
-const token = getLoacalStorage('@token');
-
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 export const InstanceWithToken = axios.create({
-  headers: { Authorization: `Bearer ${token}` },
+  headers: { Authorization: `Bearer ${getLoacalStorage('@token')}` },
   withCredentials: true,
 });
 
