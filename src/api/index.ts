de48b6c894e11f6a1543514f1f87b57a6a6685dc
nahyyun <nahyuun@naver.com>
2024-04-19 02:05:48 +0900

    feat: interceptor에 만료된 토큰 여부 조건 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,36 +1,41 @@
-// import { setLocalStorage } from '@store/localStorage';
+import { getLoacalStorage, setLocalStorage } from '@store/localStorage';
 import axios from 'axios';
-// import { getNewToken } from './auth';
+import { getNewToken } from './auth';
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 export const InstanceWithToken = axios.create({
   withCredentials: true,
+  headers: { Authorization: `Bearer ${getLoacalStorage('@token')}` },
 });
 
-// InstanceWithToken.interceptors.response.use(
-//   response => response,
-//   async error => {
-//     const originalRequest = error.config;
+InstanceWithToken.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config;
 
-//     if (error.response?.status === 401 && originalRequest.url !== '/v1/auth/refresh') {
-//       try {
-//         const response = await getNewToken();
+    if (
+      error.response?.status === 401 &&
+      error.response?.data?.error_name === 'EXPIRED_TOKEN' &&
+      originalRequest.url !== '/v1/auth/refresh'
+    ) {
+      try {
+        const response = await getNewToken();
 
-//         const { accessToken } = response.data;
+        const { accessToken } = response.data;
 
-//         originalRequest.headers.Authorization = `Bearer ${accessToken}`;
-//         InstanceWithToken.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
-//         setLocalStorage('@token', accessToken);
+        originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+        InstanceWithToken.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+        setLocalStorage('@token', accessToken);
 
-//         return InstanceWithToken(originalRequest);
-//       } catch (error) {
-//         window.location.href = '/';
-//         console.log('failed refresh token', error);
-//       }
-//     }
+        return InstanceWithToken(originalRequest);
+      } catch (error) {
+        window.location.href = '/';
+        console.log('failed refresh token', error);
+      }
+    }
 
-//     return Promise.reject(error);
-//   },
-// );
+    return Promise.reject(error);
+  },
+);
