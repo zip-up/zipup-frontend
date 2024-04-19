26aefe652235cbcbc2bdd66ccfafe4718e1d8501
nahyyun <nahyuun@naver.com>
2024-04-19 23:43:14 +0900

    feat: kakao sdk script 삽입

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -3,11 +3,18 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          src="https://t1.kakaocdn.net/kakao_js_sdk/2.2.0/kakao.min.js"
+          integrity="sha384-x+WG2i7pOR+oWb6O5GV5f1KN2Ko6N7PTGPS7UlasYWNxZMKQA63Cj/B2lbUmUfuC"
+          crossOrigin="anonymous"
+          defer
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
