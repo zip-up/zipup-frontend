79687e72a3f384777dd277c66a28ee34d5ff2493
nahyyun <nahyuun@naver.com>
2024-04-20 01:25:02 +0900

    fix: 카카오 sdk 초기화 작업

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -5,8 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <script
-          src="https://t1.kakaocdn.net/kakao_js_sdk/2.2.0/kakao.min.js"
-          integrity="sha384-x+WG2i7pOR+oWb6O5GV5f1KN2Ko6N7PTGPS7UlasYWNxZMKQA63Cj/B2lbUmUfuC"
+          src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.1/kakao.min.js"
+          integrity="sha384-kDljxUXHaJ9xAb2AzRd59KxjrFjzHa5TAoFQ6GbYTCAG0bjM55XohjjDT7tDDC01"
           crossOrigin="anonymous"
           defer
         ></script>
--- src/utils/share.ts
+++ src/utils/share.ts
@@ -4,32 +4,17 @@ interface ShareKakaoProps {
   fundId: number;
 }
 
-const registerKakaoKey = () => {
-  if (window.Kakao) {
-    const kakao = window.Kakao;
-
-    if (!kakao.isInitialized()) {
-      kakao.init(process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY);
-    }
-
-    return kakao;
-  }
-
-  return null;
-};
-
 export const shareKakao = ({ username, imageUrl, fundId }: ShareKakaoProps) => {
-  const kakao = registerKakaoKey();
-  if (!kakao) throw new Error('카카오톡을 열 수 없습니다.');
+  if (!window.Kakao) throw new Error('카카오톡 실행에 오류가 발생했습니다.');
 
   const url = `https://zip-up.vercel.app/funding/${fundId}`;
 
-  kakao.Share.sendDefault({
+  window.Kakao.Share?.sendDefault({
     objectType: 'feed',
     content: {
       title: `${username}님의 집들이에 당신을 초대합니다.`,
       description: '집업에서 선물 펀딩에 함께해주세요!',
-      imageUrl: imageUrl,
+      imageUrl,
       link: {
         mobileWebUrl: url,
       },
