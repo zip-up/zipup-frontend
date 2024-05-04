20e69031791748f39292badd3c9cc3b0d26c6f99
nahyyun <nahyuun@naver.com>
2024-05-04 19:50:48 +0900

    resolve merge conflict

--- src/components/HeaderWithLogo/index.tsx
+++ src/components/HeaderWithLogo/index.tsx
@@ -1,15 +1,15 @@
-import LogoIcon from '@assets/images/logo.svg';
-import UserIcon from '@assets/icons/user.svg';
-import { css } from 'styled-system/css';
 import { useRouter } from 'next/router';
+import UserIcon from '@assets/icons/user.svg';
+import LogoIcon from '@assets/images/logo.svg';
 import Profile from '@components/common/Profile';
 import { useUser } from '@hooks/queries/useAuth';
+import { css, cx } from 'styled-system/css';
 
 interface HeaderWithLogoProps {
   onOpen: () => void;
 }
 
-const HeaderWithLogo = ({ onOpen }: HeaderWithLogoProps) => {
+function HeaderWithLogo({ onOpen }: HeaderWithLogoProps) {
   const router = useRouter();
   const { data: user } = useUser();
 
@@ -17,14 +17,18 @@ const HeaderWithLogo = ({ onOpen }: HeaderWithLogoProps) => {
     <header className={header}>
       <div className={box} />
       <button className={logo} onClick={() => router.push('/')}>
-        <LogoIcon width={72.7} height={28} />
+        <LogoIcon />
       </button>
-      <button className={box} data-d onClick={() => (user ? router.push('/mypage') : onOpen())}>
+      <button
+        className={cx(box, css({ cursor: 'pointer' }))}
+        data-d
+        onClick={() => (user ? router.push('/mypage') : onOpen())}
+      >
         {user?.profileImage ? <Profile src={user.profileImage} size="full" /> : <UserIcon />}
       </button>
     </header>
   );
-};
+}
 
 export default HeaderWithLogo;
 
@@ -48,5 +52,4 @@ const box = css({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  cursor: 'pointer',
 });
