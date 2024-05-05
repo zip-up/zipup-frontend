64e6601ecf1989d66f7fb592a264c8476c0ef2c6
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-05-05 22:57:34 +0900

    feat: 자주 묻는 질문 페이지 구현

---
+++ src/pages/faq/index.page.tsx
@@ -0,0 +1,76 @@
+import Header from '@components/common/Header';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { style } from './styles';
+import { useForm } from 'react-hook-form';
+import SearchIcon from '../../assets/icons/search.svg';
+import { css, cx } from 'styled-system/css';
+import Accordion from '@components/Accordion';
+import { FaqQuestions, FaqQuestionsType } from '@constants/faqs';
+import Footer from '@components/Footer';
+
+type FormInputs = {
+  text: string;
+};
+
+const Faq = () => {
+  const router = useRouter();
+  const [activeTab, setActiveTab] = useState('이용문의');
+  const [keyword, setKeyword] = useState('');
+  const { register, getValues, handleSubmit } = useForm<FormInputs>();
+
+  const submitHandler = () => {
+    setKeyword(getValues('text'));
+  };
+
+  return (
+    <>
+      <Header title="자주 묻는 질문" onGoBack={() => router.back()} />
+      <form className={style.searchBox} onSubmit={handleSubmit(submitHandler)}>
+        <input
+          className={style.input}
+          {...register('text')}
+          placeholder="궁금한 내용을 검색해보세요."
+        />
+        <button type="submit" className={style.searchBtn}>
+          <SearchIcon />
+        </button>
+      </form>
+      <div>
+        {!keyword && (
+          <div className={style.tabs}>
+            {['이용문의', '배송', '취소/환불', '회원'].map(item => (
+              <div
+                key={item}
+                className={cx(
+                  style.tabItem,
+                  css({
+                    fontWeight: activeTab === item ? '600' : '400',
+                    color: activeTab === item ? 'main.blue' : 'text.200',
+                    borderBottomColor: activeTab === item ? 'main.blue' : 'gray.30',
+                  }),
+                )}
+                onClick={() => setActiveTab(item)}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        )}
+        <div className={style.content}>
+          {keyword
+            ? Object.values(FaqQuestions)
+                .flat()
+                .filter(el => el.answer.includes(keyword) || el.question.includes(keyword))
+                .map(item => <Accordion key={item.question} {...item} />)
+            : FaqQuestions[activeTab as keyof FaqQuestionsType].map(item => (
+                <Accordion key={item.question} {...item} />
+              ))}
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Faq;
---
+++ src/pages/faq/styles.ts
@@ -0,0 +1,59 @@
+import { css } from 'styled-system/css';
+import { flex } from 'styled-system/patterns';
+
+const searchBox = css({
+  width: '36rem',
+  height: '7.6rem',
+  padding: '1.8rem 1.6rem 0.8rem 1.6rem',
+  position: 'relative',
+});
+
+const input = css({
+  width: '32.8rem',
+  height: '5.2rem',
+  borderRadius: '0.8rem',
+  borderColor: 'gray.300',
+  borderWidth: '0.1rem',
+  backgroundColor: 'bg.200',
+  margin: '0 auto',
+  padding: '1.3rem 5.2rem 1.19rem 1.6rem',
+  outline: 'none',
+});
+
+const searchBtn = css({
+  position: 'absolute',
+  right: '1.6rem',
+  cursor: 'pointer',
+  height: '5.2rem',
+  width: '5rem',
+  paddingLeft: '1.2rem',
+});
+
+const tabs = flex({});
+
+const tabItem = css({
+  width: '9rem',
+  height: '4.8rem',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: 'body2',
+  borderBottomWidth: '0.2rem',
+  cursor: 'pointer',
+});
+
+const content = css({
+  backgroundColor: 'bg.200',
+  height: 'calc(100vh - 17.4rem)',
+});
+
+const style = {
+  searchBox,
+  input,
+  searchBtn,
+  tabs,
+  tabItem,
+  content,
+};
+
+export { style };
