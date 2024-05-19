57bcbd9baac85e619073684ce2f1609816e4b718
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-05-19 23:54:58 +0900

    style: 사용하지 않는 코드 지우기

--- src/pages/faq/index.page.tsx
+++ src/pages/faq/index.page.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Accordion from '@components/Accordion';
 import Header from '@components/common/Header';
--- src/pages/mypage/fundings/[types].page.tsx
+++ src/pages/mypage/fundings/[types].page.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Card from '@components/Card';
 import Button from '@components/common/Button';
