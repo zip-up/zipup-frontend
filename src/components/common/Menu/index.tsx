d366c2396f8bf21d09fc6b82bdd47e746001b62a
nahyyun <nahyuun@naver.com>
2024-05-21 15:27:17 +0900

    refactor: menu 컴포넌트 내 children 타입 유효성 검사

--- src/components/common/Menu/index.tsx
+++ src/components/common/Menu/index.tsx
@@ -1,4 +1,13 @@
-import { Children, cloneElement, PropsWithChildren, ReactNode, useState } from 'react';
+import {
+  Children,
+  cloneElement,
+  Dispatch,
+  isValidElement,
+  PropsWithChildren,
+  ReactNode,
+  SetStateAction,
+  useState,
+} from 'react';
 import { css } from 'styled-system/css';
 
 import * as style from './styles';
@@ -7,25 +16,28 @@ interface MenuProps {
   activeMenuButtonTitle: ReactNode;
   menuButtonTitle: ReactNode;
 }
-
 export default function Menu({
   activeMenuButtonTitle,
   menuButtonTitle,
   children,
 }: PropsWithChildren<MenuProps>) {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isMenuOn, setIsMenuOn] = useState(false);
 
   return (
     <div
       className={css({ pos: 'relative', color: 'text.100', textStyle: 'body1', h: 'fit-content' })}
     >
-      <button type="button" onClick={() => setIsOpen(status => !status)}>
-        {isOpen ? activeMenuButtonTitle : menuButtonTitle}
+      <button type="button" onClick={() => setIsMenuOn(status => !status)}>
+        {isMenuOn ? activeMenuButtonTitle : menuButtonTitle}
       </button>
 
-      {isOpen && (
+      {isMenuOn && (
         <div className={style.menuList}>
-          {Children.map(children, child => cloneElement(child, { setIsOpen }))}
+          {Children.map(children, child => {
+            if (isValidElement<MenuItemProps>(child)) return cloneElement(child, { setIsMenuOn });
+
+            return child;
+          })}
         </div>
       )}
     </div>
@@ -34,15 +46,15 @@ export default function Menu({
 
 interface MenuItemProps {
   onClick: () => void;
-  setIsOpen?: () => void;
+  setIsMenuOn?: Dispatch<SetStateAction<boolean>>;
 }
 
-function MenuItem({ children, onClick, setIsOpen = () => {} }: PropsWithChildren<MenuItemProps>) {
+function MenuItem({ children, onClick, setIsMenuOn = () => {} }: PropsWithChildren<MenuItemProps>) {
   return (
     <div
       onClick={() => {
         onClick();
-        setIsOpen();
+        setIsMenuOn(false);
       }}
       className={style.item}
     >
