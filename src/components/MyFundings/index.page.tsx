f94dd8fbfc31128f978f08bfbd000d626315507f
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-05-21 22:48:34 +0900

    fix: 불필요한 타입 사용 삭제 및 type 기본값 trending -> ''로 변경, type 정의 수정에 따른 텍스트 표시 코드 수정

--- src/components/MyFundings/index.page.tsx
+++ src/components/MyFundings/index.page.tsx
@@ -8,43 +8,39 @@ import { FundingInfo } from '@typings/funding';
 
 import * as style from './styles';
 
-type MainTextType = {
-  title: string;
-  noResultTitle: string;
-  noResultDesc: string;
-};
-
-export type MainTextKeys = 'my' | 'participated' | 'trending';
-
-const MAIN_TEXT: Record<MainTextKeys, MainTextType> = {
-  my: {
-    title: '내가 만든 펀딩',
-    noResultTitle: '아직 만든 펀딩이 없어요',
-    noResultDesc: '지금 바로 내가 받고 싶은 선물을 등록해보세요.',
-  },
-  participated: {
-    title: '내가 참여한 펀딩',
-    noResultTitle: '아직 참여한 펀딩이 없어요',
-    noResultDesc: '집들이를 준비하는 친구에게 집업을 알려보세요.',
-  },
-  trending: {
-    title: '지금 인기있는 펀딩',
-    noResultTitle: '지금 인기있는 펀딩이 없어요',
-    noResultDesc: '집들이를 준비하는 친구에게 집업을 알려보세요.',
-  },
-};
-
 type MyFundingsProps = {
   fundingList: FundingInfo[] | undefined;
 };
 
 export default function MyFundings({ fundingList }: MyFundingsProps) {
   const router = useRouter();
-  const type = router.query.types || 'trending';
+  const type = router.query.types as string;
+
+  const getText = () => {
+    if (type === 'my') {
+      return {
+        title: '내가 만든 펀딩',
+        noResultTitle: '아직 만든 펀딩이 없어요',
+        noResultDesc: '지금 바로 내가 받고 싶은 선물을 등록해보세요.',
+      };
+    } else if (type === 'participated') {
+      return {
+        title: '내가 참여한 펀딩',
+        noResultTitle: '아직 참여한 펀딩이 없어요',
+        noResultDesc: '집들이를 준비하는 친구에게 집업을 알려보세요.',
+      };
+    }
+
+    return {
+      title: '지금 인기있는 펀딩',
+      noResultTitle: '지금 인기있는 펀딩이 없어요',
+      noResultDesc: '집들이를 준비하는 친구에게 집업을 알려보세요.',
+    };
+  };
 
   return (
     <>
-      <Header title={MAIN_TEXT[type as MainTextKeys].title} onGoBack={() => router.back()} />
+      <Header title={getText()?.title} onGoBack={() => router.back()} />
       {fundingList?.length ? (
         <div className={style.cardContent}>
           <div className={style.flexContainer}>
@@ -55,8 +51,8 @@ export default function MyFundings({ fundingList }: MyFundingsProps) {
         </div>
       ) : (
         <NoResult
-          title={MAIN_TEXT[type as MainTextKeys].noResultTitle}
-          desc={MAIN_TEXT[type as MainTextKeys].noResultDesc}
+          title={getText()?.noResultTitle}
+          desc={getText()?.noResultDesc}
           renderButton={
             <>
               {type === 'my' && (
