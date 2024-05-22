5108d611be6c276cca2a3722c21e81ed60970689
nahyyun <nahyuun@naver.com>
2024-05-22 21:34:46 +0900

    fix: 잘못된 token으로 인한 세션 오류 시 재로그인 진행

--- src/contexts/ReactQueryContext.tsx
+++ src/contexts/ReactQueryContext.tsx
@@ -4,9 +4,15 @@ import { API_ERROR_MESSAGE } from '@constants/apiError';
 import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { CustomError } from '@typings/customError';
 import { isAxiosError } from 'axios';
+import Cookies from 'js-cookie';
 
 export default function ReactQueryClient({ children }: { children: ReactNode }) {
   const router = useRouter();
+
+  const clearToken = () => {
+    localStorage.removeItem('@token');
+    Cookies.remove('token');
+  };
 
   const handleDefaultError = (status: number) => {
     if (status === 404 || status === 500) return router.push(`/${status}`);
@@ -18,6 +24,7 @@ export default function ReactQueryClient({ children }: { children: ReactNode }) 
   const handleCustomError = (customError: CustomError) => {
     const code = customError.code as keyof typeof API_ERROR_MESSAGE;
 
+    if (code === 2006) clearToken();
     if (code === 3001 || code === 3002) return router.push('/404');
     if (code === 5002) return router.push('/500');
 
