25ad70fc7fc3ae560e98678371ff30e917621f54
nahyyun <nahyuun@naver.com>
2024-06-05 11:01:43 +0900

    feat: 탈퇴 성공, 실패 페이지의 헤더 뒤로가기 시 마이페이지로 이동

--- src/components/Layout/NoticePageLayout/index.tsx
+++ src/components/Layout/NoticePageLayout/index.tsx
@@ -1,4 +1,5 @@
 import { PropsWithChildren, ReactElement } from 'react';
+import { useRouter } from 'next/router';
 import Header from '@components/common/Header';
 
 import * as style from './styles';
@@ -15,9 +16,11 @@ export function CommonNoticePage({
   imageComponent,
   children,
 }: PropsWithChildren<CommonNoticePageProps>) {
+  const router = useRouter();
+
   return (
     <>
-      <Header />
+      <Header onGoBack={() => router.push('/mypage')} />
       <div className={style.layout}>
         <>{imageComponent}</>
         <h2 className={style.title}>{title}</h2>
