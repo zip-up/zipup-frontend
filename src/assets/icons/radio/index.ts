d24ec548fa0955a4128ddf63659b142c89330579
nahyyun <nahyuun@naver.com>
2024-09-04 19:27:54 +0900

    feat: 초대장 선택 모달 UI 작업

--- src/assets/icons/radio/index.ts
+++ src/assets/icons/radio/index.ts
@@ -1,4 +1,6 @@
 import Radio_active from './active.svg';
+import Radio_Bg_White_active from './bg-white-active.svg';
+import Radio_Bg_White_disabled from './bg-white-disabled.svg';
 import Radio_disabled from './disabled.svg';
 
-export { Radio_active, Radio_disabled };
+export { Radio_active, Radio_disabled, Radio_Bg_White_active, Radio_Bg_White_disabled };
